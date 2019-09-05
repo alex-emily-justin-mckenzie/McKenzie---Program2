0f92ef63f4c23439e5710291f8b3835379dd598b
McKenzie Humann <mhv3p@mail.umkc.edu>
2019-09-04 21:19:08 -0500

    Initial Commit

---
+++ Program2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Program2
+//
+//  Created by McKenzie Humann on 9/4/19.
+//  Copyright © 2019 McKenzie Humann. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
